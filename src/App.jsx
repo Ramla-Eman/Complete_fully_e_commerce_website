b5a64a5f785be4fec4f6eb57ea30f11d96528be4
Ramla Eman <ramla.eman.ra@gmail.com>
2025-04-07 23:56:54 +0500

    E-commerce website startup

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import components
+import Footer from "./components/Footer";
+import Topbar from "./components/Topbar";
+import Navbar from "./components/Navbar";
+// import pages
+import Home from "./pages/Home";
+import Single_Product from "./pages/Single_Product";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
+import Account from "./pages/Account";
+import Checkout from "./pages/Checkout";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Error_404 from "./pages/Error_404";
+
+const App = () => {
+  return (
+    <Router>
+      <Topbar />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/single-product" element={<Single_Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Error_404 />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/Account_Dropdown.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Account_Dropdown = () => {
+  return (
+    <div>Account_Dropdown</div>
+  )
+}
+
+export default Account_Dropdown
